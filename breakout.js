b3e57863415e40edf101c8985fc4ec164ca4517a
patrickmcmahon72 <35769415+patrickmcmahon72@users.noreply.github.com>
2018-03-07 13:09:07 +0000

    Add files via upload

--- breakout.js
+++ breakout.js
@@ -1,28 +1,104 @@
+//Setup the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 100, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+//Set the starting point
+var x = canvas.width/2;
+var y = canvas.height-30;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+if(y + dy < 0){
+	dy = -dy;
+}
 
-ctx.beginPath();
-ctx.rect(420, 270, 50, 50);
-ctx.fillStyle = "#FFFF00";
-ctx.fill();
-ctx.closePath();
+if(y + dy > canvas.height){
+	dy = -dy;
+}
 
-// This square is 50 pixels wide and 50 pixels high
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+//Draw the ball
+function drawBall()	{
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function draw()
+{
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy;
+	
+	if(y + dy > canvas.height || y + dy < 0){
+	dy = -dy;
+}
+	
+	if(x + dx > canvas.width || x +dx < 0){
+	dx = -dx;
+	}
+	
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
+	dx = -dx;
+}
+
+if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+	dy = -dy;
+}
+
+if(rightPressed && paddleX < canvas.width-paddleWidth) {
+	paddleX += 7;
+}
+else if(leftPressed && paddleX > 0) {
+	paddleX -= 7;
+}
+}
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+
+if(rightPressed) {
+	paddleX += 7;
+}
+else if (leftPressed) {
+	paddleX -= 7;
+}
+
+setInterval(draw, 10);
+
