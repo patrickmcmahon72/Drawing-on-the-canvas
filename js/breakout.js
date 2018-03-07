2160e1861c112aab8b3d9427124c6ee54fd66cc1
patrickmcmahon72 <35769415+patrickmcmahon72@users.noreply.github.com>
2018-03-07 14:32:43 +0000

    Add files via upload

--- js/breakout.js
+++ js/breakout.js
@@ -16,6 +16,42 @@ var leftPressed = false;
 var dx = 2;
 var dy = -2;
 
+//Setup some bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+//Hold the bricks in a two-dimensional array - think of it as rows and columns
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+//This function draws the bricks
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
+
+
 //Draw the ball
 function drawBall()	{
 	ctx.beginPath();
@@ -40,6 +76,8 @@ function draw()
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
+	//draw the bricks
+	drawBricks();
 	
 	
 	
@@ -49,9 +87,23 @@ function draw()
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
 	dx = -dx;
 	}
-
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+	
+	
+	//Bounce the ball off three walls - if it drops off the bottom - Game over!
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	if(y + dy < ballRadius) {
 		dy = -dy;
+	} else if(y + dy > canvas.height-ballRadius) {
+		//Check if the ball is hitting the Paddle
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else {
+		alert("GAMEOVER");
+		document.location.reload();
+	}
 	}
 
 		if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -60,6 +112,9 @@ function draw()
 	else if(leftPressed && paddleX > 0) {
 		paddleX -= 7;
 	}
+	
+	
+
 
 }
 
@@ -91,6 +146,9 @@ if(rightPressed) {
 else if (leftPressed) {
 	paddleX -= 7;
 }
+
+//Bounce the ball off three walls - if it drops off the bottom - Game over!
+	
 
 setInterval(draw, 10);
 
