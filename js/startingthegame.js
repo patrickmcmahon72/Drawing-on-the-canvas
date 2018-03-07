368212a44ea91ea5b59de44ff36e35467abeb62f
patrickmcmahon72 <35769415+patrickmcmahon72@users.noreply.github.com>
2018-03-07 13:44:16 +0000

    paddle created

---
+++ js/startingthegame.js
@@ -0,0 +1,19 @@
+//Setup the canvas
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+//Set the starting point
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+
+//Draw the ball
+function draw()	{
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+setInterval(draw, 10);
